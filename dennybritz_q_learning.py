7cd64ac0d0701630f4e67c1a77d8eec1b5d8e358
Wes Smith <wessm@cisco.com>
2018-11-23 20:49:02 -0500

    Enable exploration-based action selection

--- dennybritz_q_learning.py
+++ dennybritz_q_learning.py
@@ -5,6 +5,7 @@ import sys
 from collections import defaultdict
 
 import dennybritz_plotting as plotting
+from aima_utils import argmax
 
 
 def make_epsilon_greedy_policy(Q, epsilon, nA):
@@ -34,18 +35,46 @@ def make_epsilon_greedy_policy(Q, epsilon, nA):
 
     return policy_fn
 
+def make_exploration_function(Rplus, Ne):
+    """
+    Creates an "exploratory" policy (Exploration Function from AIMA chapter 21) based on a given Q-function and epsilon.
 
-def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
+    Args:
+        Q: A dictionary that maps from state -> action-values.
+            Each value is a numpy array of length nA (see below)
+        Nsa: A dictionary that maps state -> number of times an action has been taken
+        Rplus: Large reward value to assign before iteration limit
+        Ne: Minimum number of times that each action will be taken at each state
+        nA: Number of actions in the environment.
+
+    Returns:
+        A function that takes the observation as an argument and returns
+        the probabilities for each action in the form of a numpy array of length nA.
+
+    """
+
+    def exploration_fn(u, n):
+        if n < Ne:
+            return Rplus
+        else:
+            return u
+    return np.vectorize(exploration_fn)
+
+
+def q_learning(env, method, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1, Rplus=None, Ne=None):
     """
     Q-Learning algorithm: Off-policy TD control. Finds the optimal greedy policy
     while following an epsilon-greedy policy
 
     Args:
         env: OpenAI environment.
+        method: ['greedy', 'explore'] whether to use a greedy or an explorative policy
         num_episodes: Number of episodes to run for.
         discount_factor: Gamma discount factor.
         alpha: TD learning rate.
         epsilon: Chance the sample a random action. Float betwen 0 and 1.
+        Rplus: Optimistic reward given to unexplored states
+        Ne: Minimum number of times that each action will be taken at each state
 
     Returns:
         A tuple (Q, episode_lengths).
@@ -57,16 +86,35 @@ def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
     # A nested dictionary that maps state -> (action -> action-value).
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
 
-    # Keeps track of how many times we've taken action a in state s
-    n_sa = defaultdict(lambda: np.zeros(env.action_space.n))
-
     # Keeps track of useful statistics
     stats = plotting.EpisodeStats(
         episode_lengths=np.zeros(num_episodes),
         episode_rewards=np.zeros(num_episodes))
+    # Keeps track of how many times we've taken action a in state s
+    Nsa = defaultdict(lambda: np.zeros(env.action_space.n))
 
     # The policy we're following
-    policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
+    if method == 'greedy':
+        policy = make_epsilon_greedy_policy(Q, epsilon, env.action_space.n)
+        def get_next_action(state_):
+            action_probs = policy(state_)
+            return np.random.choice(np.arange(len(action_probs)), p=action_probs)
+    elif method == 'explore':
+        if not Rplus:
+            Rplus = max(env.reward_range)
+        if not Ne:
+            Ne = 100
+        exploration_fn = make_exploration_function(Rplus, Ne)
+        done_exploring = False
+        def get_next_action(state_):
+            exploration_values = exploration_fn(Q[state_], Nsa[state_])
+            if np.allclose(exploration_values, exploration_values[0]):
+                return np.random.randint(env.nA)
+            else:
+                return np.argmax(exploration_values)
+    else:
+        raise ValueError('Unsupported method type')
+
 
     for i_episode in range(num_episodes):
         # Print out which episode we're on, useful for debugging.
@@ -81,15 +129,14 @@ def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
         # total_reward = 0.0
         for t in itertools.count():
 
-            # Take a step
-            action_probs = policy(state)
-            action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
-            n_sa[state][action] += 1
+            # Get an action based on the exploration function
+            action = get_next_action(state)
             next_state, reward, done, _ = env.step(action)
 
             # Update statistics
             stats.episode_rewards[i_episode] += reward
             stats.episode_lengths[i_episode] = t
+            Nsa[state][action] += 1
 
             # TD Update
             best_next_action = np.argmax(Q[next_state])
@@ -97,6 +144,13 @@ def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
             td_delta = td_target - Q[state][action]
             Q[state][action] += alpha * td_delta
 
+            if method == 'explore' and not done_exploring:
+                for arr in Nsa.values():
+                    if not np.all(arr >= Ne):
+                        break
+                else:
+                    done_exploring = True
+                    print('All done with exploration at episode %i, step %i' % (i_episode, t))
             if done:
                 break
 
@@ -105,4 +159,4 @@ def q_learning(env, num_episodes, discount_factor=1.0, alpha=0.5, epsilon=0.1):
     final_policy = np.zeros((env.nS, env.nA))
     for state in range(env.nS):
         final_policy[state] = Q[state]
-    return Q, stats, n_sa, final_policy
+    return Q, stats, Nsa, final_policy
